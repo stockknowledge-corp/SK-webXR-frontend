58752d7aafcfd09fe238aa1015c5320733569179
danrayrollan <danrayrollan98@gmail.com>
2021-02-02 00:34:58 +0800

    add different models for EM-Spectrum, service-worker.js is working and saving into cache

--- public/assets/physics/em-spectrum/service-worker.js
+++ public/assets/physics/em-spectrum/service-worker.js
@@ -1,36 +1,45 @@
 // Offline Service Worker
-const staticCache = 'site-static';
+const staticCache = 'sk-site-static-offline';
 const toCache = [
-    '/assets/2d/image/glow-gradient/glow1.png',
-    '/assets/2d/image/glow-gradient/Glow02.png',
-    '/assets/2d/image/congratulations.png',
-    '/assets/2d/image/exit.png',
-    '/assets/2d/image/gamma-ray-ui.png',
-    '/assets/2d/image/infrared-ui.png',
-    '/assets/2d/image/instructions.png',
-    '/assets/2d/image/microwaves.png',
-    '/assets/2d/image/protozoa.png',
-    '/assets/2d/image/radiowaves.png',
-    '/assets/2d/image/start-learning.png',
-    '/assets/2d/image/ultraviolet-ui.png',
-    '/assets/2d/image/visible-light-ui.png',
-    '/assets/2d/image/welcome-em-2.png',
-    '/assets/2d/image/image/x-ray.png',
-    '/assets/3d/downhill/downhill.glb',
-    '/assets/3d/em-waves/EM waves v2.glb',
-    '/assets/3d/hologram/hologram.glb',
-    '/assets/audio/1.mp3',
-    '/assets/audio/2.mp3',
-    '/assets/audio/3.mp3',
-    '/assets/audio/4.mp3',
-    '/assets/audio/5.mp3',
-    '/assets/audio/6.mp3',
-    '/assets/audio/7.mp3',
-    '/assets/audio/8.mp3',
-    '/assets/audio/9.mp3',
-    '/assets/audio/10.mp3',
-    '/main.js',
-    '/main.css'
+    '/',
+    '/subject/physics/em-spectrum/main.css',
+    '/subject/physics/em-spectrum/main.js',
+    '/subject/physics/em-spectrum',
+    '/subject/physics/em-spectrum/assets/2d/image/congratulations.png',
+    '/subject/physics/em-spectrum/assets/2d/image/exit.png',
+    '/subject/physics/em-spectrum/assets/2d/image/gamma-ray-ui.png',
+    '/subject/physics/em-spectrum/assets/2d/image/infrared-ui.png',
+    '/subject/physics/em-spectrum/assets/2d/image/instructions.png',
+    '/subject/physics/em-spectrum/assets/2d/image/microwaves.png',
+    '/subject/physics/em-spectrum/assets/2d/image/radiowaves.png',
+    '/subject/physics/em-spectrum/assets/2d/image/sk-loader.gif',
+    '/subject/physics/em-spectrum/assets/2d/image/start-learning.png',
+    '/subject/physics/em-spectrum/assets/2d/image/ultraviolet-ui.png',
+    '/subject/physics/em-spectrum/assets/2d/image/visible-light-ui.png',
+    '/subject/physics/em-spectrum/assets/2d/image/welcome-em-2.png',
+    '/subject/physics/em-spectrum/assets/2d/image/x-ray.png',
+    '/subject/physics/em-spectrum/assets/2d/glow-gradient/glow1.png',
+    '/subject/physics/em-spectrum/assets/2d/glow-gradient/Glow02.png',
+    '/subject/physics/em-spectrum/assets/3d/downhill/downhill.glb',
+    '/subject/physics/em-spectrum/assets/3d/em-waves/em-waves-v2.glb',
+    '/subject/physics/em-spectrum/assets/3d/em-waves/gamma-ray.glb',
+    '/subject/physics/em-spectrum/assets/3d/em-waves/Infrared.glb',
+    '/subject/physics/em-spectrum/assets/3d/em-waves/microwaves.glb',
+    '/subject/physics/em-spectrum/assets/3d/em-waves/radiowaves.glb',
+    '/subject/physics/em-spectrum/assets/3d/em-waves/ultraviolet.glb',
+    '/subject/physics/em-spectrum/assets/3d/em-waves/Visible.glb',
+    '/subject/physics/em-spectrum/assets/3d/em-waves/xray.glb',
+    '/subject/physics/em-spectrum/assets/3d/hologram/hologram.glb',
+    '/subject/physics/em-spectrum/assets/audio/1.mp3',
+    '/subject/physics/em-spectrum/assets/audio/2.mp3',
+    '/subject/physics/em-spectrum/assets/audio/3.mp3',
+    '/subject/physics/em-spectrum/assets/audio/4.mp3',
+    '/subject/physics/em-spectrum/assets/audio/5.mp3',
+    '/subject/physics/em-spectrum/assets/audio/6.mp3',
+    '/subject/physics/em-spectrum/assets/audio/7.mp3',
+    '/subject/physics/em-spectrum/assets/audio/8.mp3',
+    '/subject/physics/em-spectrum/assets/audio/9.mp3',
+    '/subject/physics/em-spectrum/assets/audio/10.mp3'
 ];
 
 //Install server worker
@@ -39,7 +48,7 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(staticCache)
         .then(function(cache) {
-            console.log('Opened Cache');
+            //console.log('Opened Cache');
             return cache.addAll(toCache);
         })
     );
